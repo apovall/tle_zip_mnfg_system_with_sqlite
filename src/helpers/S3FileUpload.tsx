f75ea4e661977e9f33012e119eafa373c600d578
AlanP <alan.g.povall.2@gmail.com>
2024-09-16 19:59:39 +1200

    Add S3 upload feedback for success and error states

--- src/helpers/S3FileUpload.tsx
+++ src/helpers/S3FileUpload.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ipcRenderer } from 'electron';
-import { FaCloudUploadAlt } from "react-icons/fa";
+import { FaCloudUploadAlt, FaSpinner } from "react-icons/fa";
 
 function S3FileUpload() {
   const [showTooltip, setShowTooltip] = useState(false)
-
-  const handleUpload = () => {
+  const [isUploading, setIsUploading] = useState(false)
+  const [result, setResult] = useState<string>("")
+  const [resultStyle, setResultStyle] = useState<string>("text-zip-dark")
+  
+  
+  const handleUpload = async () => {
+    setResultStyle('text-zip-dark')
+    setIsUploading(true)
+    setShowTooltip(false)
+    setResult('Uploading...')
     ipcRenderer.send('upload-file', {
       accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY,
       secretAccessKey: import.meta.env.VITE_AWS_SECRET,
@@ -14,15 +22,40 @@ function S3FileUpload() {
     })
   }
 
-  const tooltip = (
-    <div className='w-1/12 bg-white text-right text-zip-dark opacity-50 mx-4'>
-      <p>Upload to S3</p>
-    </div>
-  )
+  useEffect(() => {
+    ipcRenderer.on('upload-error', (event, arg) => {
+      console.log('upload-error', arg)
+      setIsUploading(false)
+      setResult('Error uploading file. See console for details.')
+      setResultStyle('text-cancel')
+    })
+    ipcRenderer.on('upload-success', (event, arg) => {
+      console.log('upload-success', arg)
+      setIsUploading(false)
+      const dt = new Date().toLocaleString('en-NZ', { timeZone: 'Pacific/Auckland', hour12: false });
+      setResult(`Successfully uploaded at ${dt}`)
+      setResultStyle('text-acceptable-green')
+      setTimeout(() => {
+        setResult('')
+        setResultStyle('text-zip-dark')
+      }, 5000)
+    })
+
+    return () => {
+      ipcRenderer.removeAllListeners('upload-error')
+      ipcRenderer.removeAllListeners('upload-success')
+    }
+  },[])
 
   return (
     <div className='relative flex flex-row justify-end mx-4 mt-4'>
-      {showTooltip && tooltip}
+      {showTooltip && (
+        <div className={`w-2/12 bg-white text-right text-zip-dark opacity-50 mx-4`}>
+          <p>Upload to S3</p>
+        </div>
+      )}
+      {<p className={`${resultStyle} mx-4`}>{result}</p>}
+      {isUploading && <FaSpinner className='animate-spin mx-2' color='#1986A4' size={25} />}
       <FaCloudUploadAlt 
         className='cursor-pointer hover:text-zip-dark text-medium-gray transition-colors '
         size={25} 
